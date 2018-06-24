49e182547f512cd91c319326d3ad349bb3c79e32
Poyarkov S <webmaster.cipa@gmail.com>
2018-06-24 23:23:42 +0700

    check superuser privileges

--- app_setup.sh
+++ app_setup.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env bash
 
+# --------------------------------------------------------------------
+# THIS SCRIPT AUTOMATICALLY CONFIGURES DOCKER ORIENTED WEB APPLICATION
+# --------------------------------------------------------------------
+
+
+set -ae
+. ./.env
+set +a
+
+
+
+if [[ $(id -u) != 0 ]]; then
+    echo "Run script with superuser privileges!"
+    exit 1
+fi
